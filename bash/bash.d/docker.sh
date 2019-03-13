f86c4f3f914f925cd1b2ae79ec0062ab3104bf81
Philip Hallstrom <phallstrom@instructure.com>
2019-03-13 09:49:39 -0700

    don't autoload dinghy by default

--- bash/bash.d/docker.sh
+++ bash/bash.d/docker.sh
@@ -1,27 +1,3 @@
-dinghy () {
-  $(command dinghy shellinit)
-  unset -f dinghy
-  command dinghy "$@"
-}
-
-docker () {
-  if [[ -d ~/.dinghy ]]; then
-    $(command dinghy shellinit)
-    export DOCKER_MACHINE_IP=`dinghy ip`
-  fi
-  unset -f docker
-  command docker "$@"
-}
-
-docker-compose () {
-  if [[ -d ~/.dinghy ]]; then
-    $(command dinghy shellinit)
-    export DOCKER_MACHINE_IP=`dinghy ip`
-  fi
-  unset -f docker-dompose
-  command docker-compose "$@"
-}
-
 # https://github.com/docker/compose/issues/3106
 export COMPOSE_HTTP_TIMEOUT=86400
 
