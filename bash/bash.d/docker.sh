1d1ec8bac162ed1eabff8743ab51740cae8dfa49
Philip Hallstrom <philip.hallstrom@kennasecurity.com>
2019-11-26 15:22:25 -0800

    no more dinghy

--- bash/bash.d/docker.sh
+++ bash/bash.d/docker.sh
@@ -2,14 +2,3 @@
 export COMPOSE_HTTP_TIMEOUT=86400
 
 alias dc='docker-compose'
-
-if [[ -x $(which dinghy) ]]; then
-  st_mtime=0
-  eval `stat -qs $HOME/.bash_dinghy`
-  now=$(date +%s)
-  delta=$(($now - $st_mtime))
-  [[ $delta -gt 86400 ]] && dinghy shellinit > $HOME/.bash_dinghy
-  source $HOME/.bash_dinghy
-fi
-
-
