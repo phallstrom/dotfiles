d382c14f56c633e9539cf4694dd83f1ba4630aca
Philip Hallstrom <philip.hallstrom@kennasecurity.com>
2019-11-26 15:21:11 -0800

    GREP_OPTIONS is deprecated

--- bash/bash.d/001_env.sh
+++ bash/bash.d/001_env.sh
@@ -1,7 +1,6 @@
 export EDITOR=vim
 export VISUAL=vim
 export LESS="-ErX $LESS"
-export GREP_OPTIONS="--exclude=\*.svn\* --exclude=\*.git\* --exclude=\*.rsync_cache\* --exclude=*\.swp"
 
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 PATH="$HOME/bin:$PATH"
