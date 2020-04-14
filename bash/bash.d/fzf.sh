ba8e2178641ca5c870b81944b52a408271a57724
Philip Hallstrom <philip.hallstrom@kennasecurity.com>
2020-04-14 15:10:31 -0700

    if an only if we have fzf installed

--- bash/bash.d/fzf.sh
+++ bash/bash.d/fzf.sh
@@ -7,18 +7,20 @@ fi
 [[ -d "/usr/local/opt/fzf" ]] && opt_fzf=/usr/local/opt/fzf
 [[ -d "/usr/share/fzf" ]] && opt_fzf=/usr/share/fzf
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "$opt_fzf/shell/completion.bash" 2> /dev/null
+if [[ -n "$opt_fzf" ]]; then
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "$opt_fzf/shell/completion.bash" 2> /dev/null
 
-# Key bindings
-# ------------
-source "$opt_fzf/shell/key-bindings.bash"
+  # Key bindings
+  # ------------
+  source "$opt_fzf/shell/key-bindings.bash"
 
-export FZF_DEFAULT_COMMAND='ag -g ""'
-export FZF_DEFAULT_OPTS='--height 99% --reverse --color=16'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_DEFAULT_COMMAND='ag -g ""'
+  export FZF_DEFAULT_OPTS='--height 99% --reverse --color=16'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-_fzf_compgen_path() {
-  ag -g "" "$1"
-}
+  _fzf_compgen_path() {
+    ag -g "" "$1"
+  }
+fi
