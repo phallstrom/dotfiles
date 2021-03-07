64a8d29770f906bdace612802b9f8049f4329be3
Philip Hallstrom <philip.hallstrom@shop-ware.com>
2021-03-07 09:29:17 -0800

    never spring

--- bash/bash.d/001_env.sh
+++ bash/bash.d/001_env.sh
@@ -1,8 +1,10 @@
 export EDITOR=vim
 export VISUAL=vim
 export LESS="-ErX $LESS"
+export DISABLE_SPRING=1
 
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 PATH="$HOME/bin:$PATH"
 PATH="$PATH:node_modules/.bin"
 export PATH
+
