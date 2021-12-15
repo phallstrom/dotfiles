d699844e5623b8d501afb8c57a0f3bc9f9229d07
Philip Hallstrom <phallstrom@dnsfilter.com>
2021-12-15 11:17:32 -0800

    docker-compose always. also stop scanning. ugh

--- bash/bash.d/docker.sh
+++ bash/bash.d/docker.sh
@@ -1,7 +1,8 @@
 # https://github.com/docker/compose/issues/3106
 export COMPOSE_HTTP_TIMEOUT=86400
+export DOCKER_SCAN_SUGGEST=false
 
-alias dc='docker compose'
+alias dc='docker-compose'
 alias d='docker'
 
 complete -F _docker d
