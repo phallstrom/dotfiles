fed6ce6e8356f2a6a4e9a045fa7095f54959011a
Philip Hallstrom <phallstrom@dnsfilter.com>
2026-09-18 13:20:33 -0700

    fix fzf loading

--- bash/bash.d/fzf.sh
+++ bash/bash.d/fzf.sh
@@ -1,26 +1,6 @@
-# Setup fzf
-# ---------
-if [[ -d "/usr/local/opt/fzf/bin" && ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
+export FZF_DEFAULT_COMMAND='rg -l ""'
+export FZF_DEFAULT_OPTS='--height 99% --reverse --color=16'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-[[ -d "/usr/local/opt/fzf" ]] && opt_fzf=/usr/local/opt/fzf
-[[ -d "/usr/share/fzf" ]] && opt_fzf=/usr/share/fzf
+eval "$(fzf --bash)"
 
-if [[ -n "$opt_fzf" ]]; then
-  # Auto-completion
-  # ---------------
-  [[ $- == *i* ]] && source "$opt_fzf/shell/completion.bash" 2> /dev/null
-
-  # Key bindings
-  # ------------
-  source "$opt_fzf/shell/key-bindings.bash"
-
-  export FZF_DEFAULT_COMMAND='rg -l ""'
-  export FZF_DEFAULT_OPTS='--height 99% --reverse --color=16'
-  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
-  _fzf_compgen_path() {
-    rg -l "" "$1"
-  }
-fi
