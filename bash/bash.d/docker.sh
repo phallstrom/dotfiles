0a4a34ff3451ab5de6a84d4030f7e0a97c349cab
Philip Hallstrom <phallstrom@dnsfilter.com>
2026-09-18 13:20:01 -0700

    disable interactive hint menu

--- bash/bash.d/docker.sh
+++ bash/bash.d/docker.sh
@@ -1,6 +1,7 @@
 # https://github.com/docker/compose/issues/3106
 export COMPOSE_HTTP_TIMEOUT=86400
 export DOCKER_SCAN_SUGGEST=false
+export COMPOSE_MENU=false
 
 alias dc='docker compose'
 alias d='docker'
